a36ddf0e75432658aadfe6c1462df43650ac72d3
reiczel01 <reiczel@proton.me>
2023-03-29 22:21:03 +0200

    Added redirect admin icon to top-bat. Fixed logout. Not entirely implemented into the UI. Added functions for securing against XSS and sqlInjection (not implementation). Fixed database decription errors. Added page for testing session data. ################# Version 1.1.4 ######################

--- top-bar.php
+++ top-bar.php
@@ -4,5 +4,22 @@
 </style>
 <div class="top-bar">
     <object class="logo-top" data="images/logo/logo-main.svg" width="300" height="85"> </object>
-    <div class="user-icon"><a href="#" class="material-symbols-outlined">account_circle</a></div>
+    <div class="user-icon">
+
+
+        <?php
+        session_start();
+        if ($_SESSION["useris_admin"] == 1) {
+            echo '<a href="#" class="material-symbols-outlined">auto_fix</a>';
+            echo '<a href="#" class="material-symbols-outlined">account_circle</a>';
+        }
+        else
+        {
+            if(isset($_SESSION["userid"]))
+            {
+                echo '<a href="#" class="material-symbols-outlined">account_circle</a>';
+            }
+        }
+        ?>
+    </div>
 </div>
